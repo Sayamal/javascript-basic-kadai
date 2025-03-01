d8cb8df739ac5ca2a5371df6ae2dbc84e11986ab
Sayamal <165476863+Sayamal@users.noreply.github.com>
2025-03-01 13:24:37 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,13 @@
+const textElement = document.getElementById('text');
+const btnElement = document.getElementById('btn');
+
+console.log(textElement);
+
+console.log(btnElement);
+
+btnElement.addEventListener('click',() => {
+
+    setTimeout(() => {
+        textElement.textContent = 'ボタンをクリックしました';
+    }, 2000);
+});
